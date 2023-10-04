639df53888a2940be7a84b05ba60abb4bf3b2ce9
Alan Bourdoncle <alan.waret24@gmail.com>
2023-10-04 12:15:15 +0200

    Login Pop-up

--- src/javascript/login.js
+++ src/javascript/login.js
@@ -16,9 +16,16 @@ function loginButton() {
     var passValid =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
     var password = document.getElementById("loginPass").value;
     if (passValid.test(password)){
-        document.getElementById("loginBtn").disabled = false;
+        document.getElementById("loginBtn").style.display = "block";
     }else {
-        document.getElementById("loginBtn").disabled = true;
+        document.getElementById("loginBtn").style.display = "none";
     }
     
+}
+
+var  updateLogin = () => document.getElementById("login").style.display = document.getElementById("login").style.display == "block" ? "none" : "block"
+
+
+function hideLogin() {
+    document.getElementById("login").style.display = "none"
 }
