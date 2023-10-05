4a2c8522192dc8e0725413b486dad88874f1ec35
ogb4n <ogb4n@protonmail.com>
2023-10-05 02:46:17 +0200

    login component + fixes

--- src/javascript/login.js
+++ src/javascript/login.js
@@ -1,17 +1,3 @@
-function blur(){
-    document.getElementById("blurrr").style.display = "block";
-    document.getElementById("filter").style.filter = "blur(4px)";
-    document.getElementById("search").style.filter = "blur(4px)";
-    document.getElementById("post").style.filter = "blur(4px)";
-}
-
-function unblur(){
-    document.getElementById("blurrr").style.display = "none";
-    document.getElementById("filter").style.filter = "blur(0px)";
-    document.getElementById("search").style.filter = "blur(0px)";
-    document.getElementById("post").style.filter = "blur(0px)";
-}
-
 function loginButton() {
     var passValid =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
     var password = document.getElementById("loginPass").value;
@@ -24,7 +10,7 @@ function loginButton() {
 }
 
 var  updateLogin = () => document.getElementById("login").style.display = document.getElementById("login").style.display == "block" ? "none" : "block"
-
+var closeLogin = () => document.getElementById("login").style.display = "none"
 
 function hideLogin() {
     document.getElementById("login").style.display = "none"
